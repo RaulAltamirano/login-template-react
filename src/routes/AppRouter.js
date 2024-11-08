b49c441eaa1b6cefa5b3843297cbb2aa53882937
Pacman <rulomania2011@hotmail.com>
2024-11-07 20:30:16 -0600

    [UPDATE] Updated routes by new pages

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -1,31 +1,36 @@
-// src/routes/AppRoutes.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import Register from '../modules/Auth/pages/Register';
-import Home from '../modules/Dashboard/pages/Home';
 import AuthLayout from '../modules/core/AuthLayout';
 import MainLayout from '../modules/core/MainLayout';
+import AuthGuard from '../modules/auth/guards/AuthGuard';
 import Profile from '../modules/auth/pages/Profile';
 import Login from '../modules/auth/pages/Login';
+import UserList from '../modules/user/page/UserPage';
+import DockerDeploy from '../modules/dockerDeploy/page/DockerDeploy';
+import SeedCreator from '../modules/seedCreator/page/SeedCreator';
 
-function AppRoutes() {
+const AppRouter = () => {
   return (
     <Router>
       <Routes>
         {/* Rutas de autenticación */}
-        <Route element={<AuthLayout />}>
-          <Route path="/login" element={<Login />} />
-          {/* <Route path="/register" element={<Register />} /> */}
+        <Route path="/" element={<AuthLayout />}>
+          <Route path="login" element={<Login />} />
+          <Route path="create-seed" element={<SeedCreator />} />
+          <Route path="deploy-docker" element={<DockerDeploy />} />
+          <Route path="user-list" element={<UserList />} />
+          {/* Aquí puedes agregar rutas para registrar usuario u otras páginas públicas */}
         </Route>
 
-        {/* Rutas protegidas del dashboard */}
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
+        {/* Rutas protegidas */}
+        <Route element={<AuthGuard />}>
+          <Route path="/" element={<MainLayout />}>
+            <Route path="profile" element={<Profile />} />
+          </Route>
         </Route>
       </Routes>
     </Router>
   );
-}
+};
 
-export default AppRoutes;
+export default AppRouter;
